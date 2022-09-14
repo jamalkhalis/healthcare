697ff04773153467a65016afae46eef1a01e48bf
Jamal KHALIS <39938150+jamalkhalis@users.noreply.github.com>
2022-09-15 00:46:32 +0100

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,10 +20,69 @@ window.addEventListener("resize", function(event) {
 
 })
 
-var flkty = new Flickity( '.main-gallery', {
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  prevNextButtons: false,
-  autoPlay: 5000
-});
+// var flkty = new Flickity( '.main-gallery', {
+//   cellAlign: 'left',
+//   contain: true,
+//   wrapAround: true,
+//   prevNextButtons: false,
+//   autoPlay: 5000
+// });
+
+let nameFormInput = "";
+let emailFormInput = "";
+let phoneFormInput = "";
+let subjectFormInput = "";
+let messageFormInput = "";
+
+const sendMessage = document.getElementById('sendMessage');
+const nameForm = document.getElementById('nameForm');
+const emailForm = document.getElementById('emailForm');
+const phoneForm = document.getElementById('phoneForm');
+const subjectForm = document.getElementById('subjectForm');
+const messageForm = document.getElementById('messageForm');
+
+
+nameForm.addEventListener('input', function(event) {
+  nameFormInput = event.target.value;
+})
+
+emailForm.addEventListener('input', function(event) {
+  emailFormInput = event.target.value;
+})
+
+phoneForm.addEventListener('input', function(event) {
+  phoneFormInput = event.target.value;
+})
+
+subjectForm.addEventListener('input', function(event) {
+  subjectFormInput = event.target.value;
+})
+
+messageForm.addEventListener('input', function(event) {
+  messageFormInput = event.target.value;
+})
+
+
+sendMessage.addEventListener('click', function(event) {
+
+  let messageObject = {
+    name: nameFormInput,
+    emailAddress: emailFormInput,
+    phone: phoneFormInput,
+    subject: subjectFormInput,
+    message: messageFormInput
+  }
+
+  fetch("http:50.62.180.70:8081/contact", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(messageObject)
+  })
+  .then(response => response.text())
+  .then(result => {
+    console.log(result)
+  })
+
+
+
+})
